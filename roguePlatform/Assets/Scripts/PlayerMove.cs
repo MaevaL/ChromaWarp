bf57d58e806b04df2a291b1472c12e600ada843e
JEREMY OTHENIN <jothenin@adlyon2.univ-lyon2.fr>
2017-10-17 10:19:23 +0200

    deplacement perso + jump + struct

--- roguePlatform/Assets/Scripts/PlayerMove.cs
+++ roguePlatform/Assets/Scripts/PlayerMove.cs
@@ -2,67 +2,58 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMove : MonoBehaviour {
-    [SerializeField]
-    private float speed;
-    private Rigidbody2D rigidBody;
-    private bool isFlying;
-    private int nbJump;
+namespace RoguePlateformer {
+    public class PlayerMove : MonoBehaviour {
+        [SerializeField]
+        private float speed;
+        [SerializeField]
+        private float speedJump;
+        private Rigidbody2D rigidBody;
+        private bool isFlying;
+        private int nbJump;
 
 
+        public void Start() {
+            speed = 4f;
+            rigidBody = GetComponent<Rigidbody2D>();
+            isFlying = false;
+            nbJump = 0;
 
-    // Use this for initialization
-    void Start() {
-        speed = 0.08f;
-        rigidBody = GetComponent<Rigidbody2D>();
-        isFlying = false;
-        nbJump = 0;
-
-    }
-
-    // Update is called once per frame
-    void FixedUpdate() {
-        if (Input.GetKey(KeyCode.UpArrow)) {
-            transform.position += new Vector3(0, 0, speed);
         }
 
-        if (Input.GetKey(KeyCode.DownArrow)) {
-            transform.position -= new Vector3(0, 0, speed);
-        }
+        public void FixedUpdate() {
+            float horizontalSpeed = 0;
 
-        if (Input.GetKey(KeyCode.LeftArrow)) {
-            transform.position -= new Vector3(speed, 0, 0);
-        }
+            horizontalSpeed += speed * Input.GetAxisRaw("Horizontal");
 
-        if (Input.GetKey(KeyCode.RightArrow)) {
-            transform.position += new Vector3(speed, 0, 0);
-        }
+            Vector2 newVelocity = rigidBody.velocity;
 
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            if (!isFlying || nbJump < 1) {
-                rigidBody.velocity = new Vector3(0, 5f, 0);
-                isFlying = true;
-                nbJump++;
+            if (Input.GetButtonDown("Jump")) {
+                if (nbJump < 2) {
+                    newVelocity.y = speedJump;
+                    isFlying = true;
+                    nbJump++;
+                }
             }
-        }  
 
-    }
+            newVelocity.x = horizontalSpeed;
+            rigidBody.velocity = newVelocity;
 
-    void OnCollisionEnter2D(Collision2D collisionInfo) {
-        foreach (ContactPoint2D contact in collisionInfo.contacts) {
-            if (contact.normal.y == 1f) {
-                isFlying = false;
-                nbJump = 0;
+        }
+
+        void OnCollisionEnter2D(Collision2D collisionInfo) {
+            foreach (ContactPoint2D contact in collisionInfo.contacts) {
+                if (contact.normal.y == 1f) {
+                    isFlying = false;
+                    nbJump = 0;
+                }
             }
         }
+
+        void OnCollisionExit2D(Collision2D collisionInfo) {
+            isFlying = true;
+            if (nbJump == 0) {  nbJump = 1;   }
+        }
+
     }
-
-    void OnCollisionExit2D(Collision2D collisionInfo) {
-        isFlying = true;
-    }
-
-
-
-
-
 }
