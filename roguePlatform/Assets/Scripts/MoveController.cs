b11f9b372497cbe1a286fd43e0c9d50cdcbbd0d7
Unknown <matthieu.eginard@gmail.com>
2017-10-17 23:12:01 +0200

    Update MoveController

--- roguePlatform/Assets/Scripts/MoveController.cs
+++ roguePlatform/Assets/Scripts/MoveController.cs
@@ -5,16 +5,17 @@ using UnityEngine;
 public class MoveController : MonoBehaviour {
 
     [SerializeField]
-    private float speed;
+    private float speed =2f;
     [SerializeField]
     private float speedJump;
     [SerializeField]
     private float groundRadius;// 0.2f;
-
+    [SerializeField]
+    private bool facingRight = true; 
     private Rigidbody2D rigidBody;
 
     private bool isFlying;
-    private bool facingRight;
+    
 
     private int nbJump;
 
@@ -26,12 +27,9 @@ public class MoveController : MonoBehaviour {
 
 
     public void Start()
-    {
-        speed = 2f;
+    { 
         rigidBody = GetComponent<Rigidbody2D>();
         isFlying = false;
-        facingRight = true;
-        nbJump = 0;
         anim = GetComponent<Animator>();
     }
 
@@ -40,40 +38,24 @@ public class MoveController : MonoBehaviour {
 
         //Check if is on the ground 
         isFlying = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
-        anim.SetBool("Ground", isFlying);
+        anim.SetBool("Ground", !isFlying);
 
-        //Jump Animation 
+        //Velocity Animation ... Idle, Walking, Running
         anim.SetFloat("vSpeed", rigidBody.velocity.y);
 
         //Adjust the velocity
         float move = -1;
-
         rigidBody.velocity = new Vector2(move * speed, rigidBody.velocity.y);
         anim.SetFloat("Speed", Mathf.Abs(move));
 
         //Change Face Direction 
+        Debug.Log(move); 
         if (move > 0 && !facingRight) { Flip(); }
         else if (move < 0 && facingRight) { Flip(); }
 
-        JumpCheck();
-
-        // dash 
-        if (Input.GetButtonDown("Dash"))
-        {
-            float dash = Input.GetAxis("Dash");
-            rigidBody.AddForce(new Vector2(speed * 100, 0));
-        }
-
     }
 
-    private void Update()
-    {
-
-
-
-
-    }
-
+    //Flip animation 
     private void Flip()
     {
         facingRight = !facingRight;
