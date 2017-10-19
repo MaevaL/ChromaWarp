52ccd812000261351e2450e7fe79ae5165321c16
Unknown <matthieu.eginard@gmail.com>
2017-10-19 16:55:23 +0200

    FireScript without collision

--- roguePlatform/Assets/Scripts/FireController.cs
+++ roguePlatform/Assets/Scripts/FireController.cs
@@ -3,24 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FireController : MonoBehaviour {
-    [SerializeField]
-    private float projectileSpeed = 5f;
+
     public GameObject projectile;
-    public GameObject Emitter; 
-    private Vector2 position; 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if(Input.GetButtonDown("Jump"))
+    public Vector2 velocity;
+    bool canShoot = true;
+    public Vector2 offset = new Vector2(0.4f, 0.1f);
+    public float cooldown = 1f;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Fire") && canShoot)
         {
-            position = new Vector2(Emitter.transform.position.x +0.1f, Emitter.transform.position.y); 
-            GameObject bullet = GameObject.Instantiate(projectile, position , Quaternion.identity);
-            bullet.transform.Translate(new Vector3(0, 1) * Time.deltaTime * projectileSpeed);
-          
+
+            GameObject go = (GameObject) Instantiate(projectile, (Vector2)transform.position + offset * transform.localScale.x, Quaternion.identity);
+            go.GetComponent<Rigidbody2D>().velocity = new Vector2(velocity.x * transform.localScale.x, velocity.y);
+            StartCoroutine(CanShoot());
+
         }
-	}
+    }
+
+    IEnumerator CanShoot()
+    {
+        canShoot = false;
+        yield return new WaitForSeconds(cooldown);
+        canShoot = true;
+    }
+
 }
