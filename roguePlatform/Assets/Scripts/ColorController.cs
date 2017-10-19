59e649a7841c822f4fc44ab5778e5c0fbf65d29f
JEREMY OTHENIN <jothenin@adlyon2.univ-lyon2.fr>
2017-10-19 14:54:58 +0200

    color controller

---
+++ roguePlatform/Assets/Scripts/ColorController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorController : MonoBehaviour {
+
+    [SerializeField]
+    private int _color = 1;
+
+    public int GetColor() {
+        return _color;
+    }
+
+    public void SwapColor() {
+
+        if (_color == 1) {
+            _color = 2;
+        }
+        else if (_color == 2) {
+            _color = 1;
+        }
+
+    }
+
+    public bool SameColor(int color) {
+
+        if (_color == color) {
+            return true;
+        }
+        else if (_color == 0 || color == 0) {
+            return true;
+        }
+
+        return false;
+
+    }
+
+}
--- roguePlatform/Assets/Scripts/Player/PlayerController.cs
+++ roguePlatform/Assets/Scripts/Player/PlayerController.cs
@@ -5,50 +5,77 @@ using UnityEngine.UI;
 using System;
 
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
     LifeController lifeController;
     public Text healthText;
     GoldController goldController;
     public Text CoinText;
     public GameObject healthBonus;
+    ColorController colorController;
 
     // Use this for initialization
-    void Start() {
+    void Start()
+    {
 
         lifeController = gameObject.GetComponent<LifeController>();
         healthText = GameObject.Find("HealthText").GetComponent<Text>();
-        healthText.text = ": "+ lifeController.GetLife();
+        healthText.text = ": " + lifeController.GetLife();
 
         goldController = gameObject.GetComponent<GoldController>();
         CoinText = GameObject.Find("CoinText").GetComponent<Text>();
         CoinText.text = ": " + goldController.GetGold();
 
+        colorController = gameObject.GetComponent<ColorController>();
+        if(colorController.GetColor() == 1) {
+            this.GetComponent<SpriteRenderer>().color = new Color(0.4f, 0.4f, 1f, 1f);
+        }
+        else {
+            this.GetComponent<SpriteRenderer>().color = new Color(1f, 0.4f, 0.4f, 1f);
+        }
+        
     }
 
     // Update is called once per frame
-    void Update() {
+    void Update()
+    {
         healthText.text = ": " + (lifeController.GetLife());
         CoinText.text = ": " + goldController.GetGold();
+
+        if (Input.GetButtonDown("SwapColor")){
+            colorController.SwapColor();
+            if (colorController.GetColor() == 1) {
+                this.GetComponent<SpriteRenderer>().color = new Color(0.4f, 0.4f, 1f, 1f);
+            }
+            else {
+                this.GetComponent<SpriteRenderer>().color = new Color(1f, 0.4f, 0.4f, 1f);
+            }
+        }
+
+
     }
 
-    void OnTriggerEnter2D(Collider2D collider) {
+    void OnTriggerEnter2D(Collider2D collider)
+    {
         /*
          * without that asteroid is destroyed at the very 1st frame
          * by boundary's which declench our triggerEnter
          */
         if (collider == null) { return; }
 
-        if (collider.CompareTag("Enemy")) {
-            Instantiate(healthBonus , transform.position + new Vector3(1,0,0) , Quaternion.identity);
+        if (collider.CompareTag("Enemy"))
+        {
+            Instantiate(healthBonus, transform.position + new Vector3(1, 0, 0), Quaternion.identity);
             Destroy(collider.gameObject);
         }
 
-        if (collider.CompareTag("HealthBonus")) {
+        if (collider.CompareTag("HealthBonus"))
+        {
 
             collider.gameObject.GetComponent<HealthBonus>().SetBonus(1);
             Destroy(collider.gameObject);
-            
+
         }
 
         if (collider.CompareTag("GoldBonus"))
