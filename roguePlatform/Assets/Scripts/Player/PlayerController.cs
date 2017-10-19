22e1b6e9ab5fdbb80f921100141df1b539aaaf14
Maeva <maeva.lauzier@gmail.com>
2017-10-19 14:42:26 +0200

    Maj les bonus gère leur effet, les enemies gère l'instantiation de leur bonus, explosion et destruction

--- roguePlatform/Assets/Scripts/Player/PlayerController.cs
+++ roguePlatform/Assets/Scripts/Player/PlayerController.cs
@@ -11,8 +11,6 @@ public class PlayerController : MonoBehaviour {
     public Text healthText;
     GoldController goldController;
     public Text CoinText;
-    public GameObject healthBonus;
-    public GameObject goldBonus; 
 
     // Use this for initialization
     void Start() {
@@ -34,45 +32,6 @@ public class PlayerController : MonoBehaviour {
     }
 
     void OnTriggerEnter2D(Collider2D collider) {
-        /*
-         * without that asteroid is destroyed at the very 1st frame
-         * by boundary's which declench our triggerEnter
-         */
         if (collider == null) { return; }
-        //TODO : Resolve Collision Problem (spawn two element, detect two collision sometimes  ) 
-        if (collider.CompareTag("Enemy")) {
-
-            float rnd = UnityEngine.Random.Range(0, 2);
-            
-            if (rnd == 1) {
-                Instantiate(healthBonus, transform.position + new Vector3(1, 0, 0), Quaternion.identity);
-            }
-            else {
-                Instantiate(goldBonus, transform.position + new Vector3(3, 0, 0), Quaternion.identity);
-            }
-          
-            Destroy(collider.gameObject);
-        }
-
-        if (collider.CompareTag("HealthBonus")) {
-
-            collider.gameObject.GetComponent<HealthBonus>().SetBonus(1);
-            Destroy(collider.gameObject);
-    
-        }
-
-        if (collider.CompareTag("GoldBonus"))
-        {
-
-            collider.gameObject.GetComponent<GoldBonus>().SetBonus(1);
-            Destroy(collider.gameObject);
-
-        }
-
-        /*
-         * Destroy gameobject's script attach and his children
-         */
-
     }
-
 }
