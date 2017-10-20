bee5fcfb93decd13b9b6652ded3fcae3f55c6d51
Unknown <matthieu.eginard@gmail.com>
2017-10-20 11:19:33 +0200

    Enemy script update, destroy only when no life

--- roguePlatform/Assets/Scripts/Enemy/Enemy.cs
+++ roguePlatform/Assets/Scripts/Enemy/Enemy.cs
@@ -21,24 +21,30 @@ public class Enemy : MonoBehaviour {
         else {
             this.GetComponent<SpriteRenderer>().color = new Color(1f , 0f , 0f , 1f);
         }
+        lifeController = gameObject.GetComponent<LifeController>(); 
 
     }
 
     private void OnTriggerEnter2D(Collider2D collider) {
         //TODO : ajuster le tag après l'implémentation des projectiles)
         if (collider.CompareTag("Projectile")) {
-            Instantiate(explosion , transform.position , Quaternion.identity);
-            Destroy(gameObject);
+            lifeController.LoseLife(1); 
+            if (lifeController.GetLife() <= 0)
+            {
+                Instantiate(explosion, transform.position, Quaternion.identity);
+                Destroy(gameObject);
 
-            float rnd = UnityEngine.Random.Range(0 , 10);
+                float rnd = UnityEngine.Random.Range(0, 10);
 
-            if (rnd == 1) {
-                Instantiate(healthBonus , transform.position + new Vector3(1 , 0 , 0) , Quaternion.identity);
-            }
-            else {
-                Instantiate(goldBonus , transform.position + new Vector3(3 , 0 , 0) , Quaternion.identity);
-            }
-            
+                if (rnd == 1)
+                {
+                    Instantiate(healthBonus, transform.position + new Vector3(1, 0, 0), Quaternion.identity);
+                }
+                else
+                {
+                    Instantiate(goldBonus, transform.position + new Vector3(3, 0, 0), Quaternion.identity);
+                }
+            }   
         }
     }
 }
