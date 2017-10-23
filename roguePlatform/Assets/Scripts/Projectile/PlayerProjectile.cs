e53651aa4f2680415ed0e2bd67441c4a0ffa34a9
Maeva <maeva.lauzier@gmail.com>
2017-10-23 16:42:10 +0200

    ajustement position noImpact avec le nouveau player

--- roguePlatform/Assets/Scripts/Projectile/PlayerProjectile.cs
+++ roguePlatform/Assets/Scripts/Projectile/PlayerProjectile.cs
@@ -7,7 +7,7 @@ public class PlayerProjectile : Projectile {
     public GameObject ImpactFX;
     protected override void SpecificCollision(LifeController lifeController, Collision2D col) {
         if(lifeController != null && lifeController.CompareTag("Enemy")) {
-            GameObject go = Instantiate(ImpactFX , transform.position , transform.rotation) as GameObject;
+            GameObject go = Instantiate(ImpactFX , transform.position, transform.rotation) as GameObject;
             lifeController.LoseLife(Damages);
         }
     }
