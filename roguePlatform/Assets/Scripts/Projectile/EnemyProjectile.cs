271cfb945421be7551f3720a745ebea56177f3a2
Maeva <maeva.lauzier@gmail.com>
2017-10-23 16:13:36 +0200

    anim impact + non impact à gerer ensuite avec les couleurs

--- roguePlatform/Assets/Scripts/Projectile/EnemyProjectile.cs
+++ roguePlatform/Assets/Scripts/Projectile/EnemyProjectile.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class EnemyProjectile : Projectile {
 
+    public GameObject ImpactFX;
+
     protected override void SpecificCollision(LifeController lifeController, Collision2D col) {
         if (lifeController != null && lifeController.CompareTag("Player")) {
+            GameObject go = Instantiate(ImpactFX , col.collider.transform.position , transform.rotation) as GameObject;
             lifeController.LoseLife(Damages);
         }
     }
