ce7cbc00688b207c69e9ec4fe9c816ef44533f9c
Maeva <maeva.lauzier@gmail.com>
2017-10-24 09:42:06 +0200

    merge master incoming

--- roguePlatform/Assets/Scripts/Projectile/PlayerProjectile.cs
+++ roguePlatform/Assets/Scripts/Projectile/PlayerProjectile.cs
@@ -6,15 +6,10 @@ public class PlayerProjectile : Projectile {
 
     public GameObject ImpactFX;
     public GameObject NoImpactFX;
-    public GameObject Player;
     protected override void SpecificCollision(LifeController lifeController, Collision2D col) {
         if(lifeController != null && lifeController.CompareTag("Enemy")) {
             ColorController EnemyColor = col.collider.gameObject.GetComponent<ColorController>();
-            ColorController PlayerColor = Player.gameObject.GetComponent<ColorController>();
-
-            Debug.Log(PlayerColor.GetColor());
-            Debug.Log(EnemyColor.GetColor());
-            Debug.Log(PlayerColor.SameColor(EnemyColor.GetColor()));
+            ColorController PlayerColor = GameObject.FindWithTag("Player").GetComponent<ColorController>();
 
             if (PlayerColor.SameColor(EnemyColor.GetColor())) {
                 GameObject go = Instantiate(ImpactFX , transform.position , transform.rotation) as GameObject;
