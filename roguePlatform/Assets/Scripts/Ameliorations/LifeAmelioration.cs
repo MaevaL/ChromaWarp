bd13dac76b18f7749cf23042374387873ec969b3
Unknown <matthieu.eginard@gmail.com>
2017-10-25 17:35:42 +0200

    Error in LifeAmelioration and update Robot Prefab

--- roguePlatform/Assets/Scripts/Ameliorations/LifeAmelioration.cs
+++ roguePlatform/Assets/Scripts/Ameliorations/LifeAmelioration.cs
@@ -18,8 +18,8 @@ public class LifeAmelioration : Amelioration {
     public override void setAmelioration() {
         Debug.Log("life"); 
         LifeController life = GameObject.FindWithTag("Player").GetComponent<LifeController>();
-        life.SetLifeMax(System.Convert.ToInt32(param));
-        life.SetLife(System.Convert.ToInt32(param));
+        //life.SetLifeMax(System.Convert.ToInt32(param));
+        //life.SetLife(System.Convert.ToInt32(param));
         //Debug.Log(life.GetLifeMax());
         //Debug.Log(life.GetLife());
     }
