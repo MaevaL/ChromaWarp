42d2c52f4982da20296067ab57411f77c708d292
Xenation <xenations.111@gmail.com>
2017-10-27 20:53:08 +0200

    Improved minimap rendering

--- roguePlatform/Assets/Scripts/Map/Minimap.cs
+++ roguePlatform/Assets/Scripts/Map/Minimap.cs
@@ -2,17 +2,21 @@
 
 namespace RoguePlateformer {
 	public class Minimap : MonoBehaviour {
-
-		public RenderTexture renderTexture;
+		
 		public Camera minimapCam;
-		public Transform player;
+		public Transform mainCam;
 
 		public void Start() {
 			
 		}
 
 		public void LateUpdate() {
-			minimapCam.transform.position = player.position + Vector3.forward * -10;
+			minimapCam.transform.position = new Vector3(mainCam.position.x, mainCam.position.y, -20f);
+			//Vector3 fake = (Vector2) MapManager.I.Grid.GetChunkAtWorldPos(mainCam.position).FakePos;
+			//fake.x = fake.x * MapManager.I.chunkWidth + MapManager.I.chunkWidth / 2;
+			//fake.y = fake.y * MapManager.I.chunkHeight + MapManager.I.chunkHeight / 2;
+			//fake.z = -20f;
+			//minimapCam.transform.position = fake;
 		}
 
 
