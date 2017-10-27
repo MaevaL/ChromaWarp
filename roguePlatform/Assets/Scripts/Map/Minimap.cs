b3755b6a34c93ad4ea272455b1d9768332654a8a
Xenation <xenations.111@gmail.com>
2017-10-27 17:24:51 +0200

    Basic minimap

--- roguePlatform/Assets/Scripts/Map/Minimap.cs
+++ roguePlatform/Assets/Scripts/Map/Minimap.cs
@@ -4,11 +4,17 @@ namespace RoguePlateformer {
 	public class Minimap : MonoBehaviour {
 
 		public RenderTexture renderTexture;
+		public Camera minimapCam;
+		public Transform player;
 
 		public void Start() {
 			
 		}
-		
+
+		public void LateUpdate() {
+			minimapCam.transform.position = player.position + Vector3.forward * -10;
+		}
+
 
 	}
 }
