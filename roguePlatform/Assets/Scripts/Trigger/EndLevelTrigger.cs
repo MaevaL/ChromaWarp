7ebc95b1401544abecc6de0bc88cfd1c733a988e
Unknown <matthieu.eginard@gmail.com>
2017-10-30 12:23:55 +0100

    EndLevel Trigger

---
+++ roguePlatform/Assets/Scripts/Trigger/EndLevelTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndLevelTrigger : MonoBehaviour {
+
+    private GameObject player;
+    private PlayerController playerController; 
+    
+	// Use this for initialization
+	private void Awake () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerController = player.GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+            playerController = player.GetComponent<PlayerController>();
+
+            playerController.SetEndLevel(true); 
+            
+        }
+    }
+
+}
